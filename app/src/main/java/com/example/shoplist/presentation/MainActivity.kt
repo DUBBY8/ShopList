796b8219b0fc948a4775bdd0adff43749dc58b73
Ivan <smirnovivan228007@gmail.com>
2024-11-20 19:03:28 +0300

    - halfLife version

--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.shoplist.presentation
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shoplist.R
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
-    private var count = 0
+    private lateinit var adapter: ShopListAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupRecyclerView()
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
-            Log.d("MainActivityTest", it.toString())
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
-            }
+            adapter.shopList = it
         }
     }
+
+    private fun setupRecyclerView() {
+        val rvShopList = findViewById<RecyclerView>(R.id.rv_shop_list)
+        val adapter = ShopListAdapter()
+        rvShopList.adapter = adapter
+    }
+
 }
